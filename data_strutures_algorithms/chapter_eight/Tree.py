68b2a1b4445bb567630ef4c9d117e69448f6e11b
min.zhang <a991391009@gmail.com>
2018-11-27 22:07:34 +0800

    finish class Tree

--- data_strutures_algorithms/chapter_eight/Tree.py
+++ data_strutures_algorithms/chapter_eight/Tree.py
@@ -15,6 +15,24 @@ class Tree:
 
         def __ne__(self, other):
             '''Return True if other does not represent the same location'''
-            return not (self == other)                  #opposite of __eq__
+            return not (self == other)  # opposite of __eq__
 
-    
+    def root(self):
+        '''Return Position representing the tree's root(or None if empty)'''
+        raise NotImplementedError('must be implemented by subclass')
+
+    def parent(self):
+        '''Return Position representing p's parent(or None if p is root)'''
+        raise NotImplementedError('must be implemented by subclass')
+
+    def num_children(self, p):
+        '''Return the number of children that Position p has'''
+        raise NotImplementedError('must be implemented by subclass')
+
+    def children(self, p):
+        '''Generate an iteration of Positions representing p's children'''
+        raise NotImplementedError('must be implemented by subclass')
+
+    def __len__(self):
+        '''Return the total number of elements in the tree'''
+        raise NotImplementedError('must be implemented by subclass')
