38fb177c4b9592a8b88290433b2b3e05ded19b66
min.zhang <a991391009@gmail.com>
2018-12-01 23:09:44 +0800

    write an abstraction representing the location of a single element, add __init__ element __eq__ functions

--- data_strutures_algorithms/chapter_eight/LinkedBinaryTree.py
+++ data_strutures_algorithms/chapter_eight/LinkedBinaryTree.py
@@ -3,11 +3,28 @@ import BinaryTree
 class LinkedBinaryTree(BinaryTree):
     '''Linked representation of  a binary tree structure'''
 
-    class _Node:
+    class _Node:             #Lightweight, nonpublic class for storing a node
         __slots__ = '_element','_parent','_left','_right'
         def __init__(self, element, parent=None, left=None,right=None):
             self._element = element
             self._parent = parent
             self._left = left
             self._right = right
+
+    class Position(BinaryTree.Position):
+        '''An abstract representing the location of a single element'''
+
+        def __init__(self, container, node):
+            self._container  = container
+            self._node = node
+
+        def element(self):
+            '''Return the element stored at this Position'''
+            return self._node._element
+
+        def __eq__(self, other):
+            '''Return True if other is a Position representing a same location'''
+            return type(other) is type(self) and other._node is self._node
+
         
+
