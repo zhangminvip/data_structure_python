fac59eadac71898d522506c67d6d74c6e1c2d9d2
min.zhang <a991391009@gmail.com>
2018-12-11 19:53:39 +0800

    __init__()

---
+++ data_strutures_algorithms/chapter_six/array_stack.py
@@ -0,0 +1,8 @@
+class ArrayStack:
+    '''LIFO Stack  implementation using a Python list as underlying storage'''
+
+    def __init__(self):
+        '''Create an empty stack'''
+        self._data = []
+
+    
