a763f57d9a1f4f70b09128102523ae487845a50c
min.zhang <a991391009@gmail.com>
2018-12-11 20:00:23 +0800

    top()

--- data_strutures_algorithms/chapter_six/array_stack.py
+++ data_strutures_algorithms/chapter_six/array_stack.py
@@ -17,5 +17,16 @@ class ArrayStack:
         '''Add element e to the top of the stack'''
         self._data.append(e)                    # new item stored at end of list
 
-    
+    def top(self):
+        '''Return (but do not remove) the element at the top of the stack
+
+        Return Empty exception if the stack is empty
+        '''
+
+        if self.is_empty():
+            raise Empty('Stack is empty')
+        return self._data[-1]                   #the last item in the list
+
+        
+
 
