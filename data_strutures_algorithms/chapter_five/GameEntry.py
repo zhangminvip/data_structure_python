9fa36141b511d927dd27bc1c838c43355548c4e9
min.zhang <a991391009@gmail.com>
2018-12-14 21:25:12 +0800

    Core algorithm : consider adding entry to high scores

--- data_strutures_algorithms/chapter_five/GameEntry.py
+++ data_strutures_algorithms/chapter_five/GameEntry.py
@@ -38,4 +38,18 @@ class Scoreboard:
         '''Consider adding entry to high scores'''
         score = entry.get_score()
 
-        
+        # Does new entry qualify as a high score
+        # answer is yes if board not full or score is higher than last entry
+        good = self._n < len(self._board) or score > self._board[-1].get_score()
+        if good:
+            if self._n < len(self._board):
+                self._n += 1
+
+            # shift lower scores rightward to make room for new entry
+            j = self._n - 1
+            while j > 0 and self._board[j-1].get_score() < score:
+                self._board[j] = self._board[j-1]
+                j -= 1
+            self._board[j] = entry
+
+
