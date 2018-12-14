74d5071cc30060fd43ff89b7c73d54fcc1f4b468
min.zhang <a991391009@gmail.com>
2018-12-14 18:55:47 +0800

    Insert value at index k, shifting subsequent values rightward

--- data_strutures_algorithms/chapter_five/dynamic_array.py
+++ data_strutures_algorithms/chapter_five/dynamic_array.py
@@ -40,3 +40,14 @@ class DynamicArray:
         '''Return new array with capacity c'''
 
         return (c * ctypes.py_object)()                  # see ctypes documentation
+
+    def insert(self, k, value):
+        '''Insert value at index k, shifting subsequent values rightward'''
+        # (for simplicity, we assume 0 <= k <= n in this version)
+        if self._n == self._capacity:                    # not enough room
+            self._resize(2 * self._capacity)             # so double capacity
+        for j in range(self._n, k, -1):
+            self._A[j] = self._A[j -1]
+        self._A[k] = value
+        self._n += 1
+
