46b8e11c49acb8b97b947cb34236450aa1387613
min.zhang <a991391009@gmail.com>
2018-12-15 23:38:16 +0800

    Insert sort

--- data_strutures_algorithms/chapter_five/dynamic_array.py
+++ data_strutures_algorithms/chapter_five/dynamic_array.py
@@ -63,3 +63,13 @@ class DynamicArray:
                 return                                   # exit immediately
         raise ValueError('Value not found')              # only reached if no match
 
+    def insertion_sort(A):
+        '''Sort list of comparable elements into nondecreasing order'''
+        for k in range(1, len(A)):
+            cur = A[k]                                   # current element to be inserted
+            j = k                                        # find correct index j for current
+            while j > 0 and A[j-1] > cur:                # element A[j-1] must be after current
+                A[j] = A[j-1]
+                j -= 1
+            A[j] = cur                                   # cur now in the right place
+
