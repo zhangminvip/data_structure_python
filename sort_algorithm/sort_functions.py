448186191b463aed27ccca015d02d0b968dfe108
min.zhang <a991391009@gmail.com>
2018-12-18 19:15:20 +0800

    recursive implementation of quick sorting

--- sort_algorithm/sort_functions.py
+++ sort_algorithm/sort_functions.py
@@ -9,3 +9,28 @@ def selectionSort(alist):
         tmp = alist[j]
         alist[i] = min
         alist[index] = tmp
+
+def quicksort(nums):
+    if len(nums) <= 1:
+        return nums
+
+    # left subtree
+    less = []
+
+    # right subtree
+    greater = []
+
+    # base number
+    base = nums.pop()
+
+    for x in nums:
+        if x < base:
+            less.append(x)
+        else:
+            greater.append(x)
+
+    return quicksort(less) + [base] + quicksort(greater)
+
+nums = [6,1,2,7,9,3,4,5,10,8,8]
+
+print(quicksort(nums))
